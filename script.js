0093ce06ebfb26ca8bd0b013363ebf9166cc4340
JOANIR FERNANDO RIBEIRO <108032158+JoanirRibeiro@users.noreply.github.com>
2025-07-30 16:13:04 -0300

    .

--- script.js
+++ script.js
@@ -18,4 +18,20 @@ console.log(linguagens[2]) // “C++”
 const lista = document.querySelector("#lista");  //seleciona elemento com id "lista" e guarda na variável "lista"
 lista.textContent = linguagens[0]; // altera o elemento HTML da variável "lista" para o valor 0 da lista "linguagens"
 lista.textContent = linguagens[1]; 
-lista.textContent = linguagens[2]; 
+lista.textContent = linguagens[2];
+
+let aluno1 = {
+  nome: "Joanir",
+  idade: 43,
+  anoLetivo: "Universidade",
+  materiasFavoritas: ["Filsofia","Programação","História"]
+}
+
+console.log(aluno1.nome);
+console.log(aluno1.idade)
+console.log(aluno1.anoLetivo);
+console.log(aluno1.materiasFavoritas[1]);
+
+
+const objeto = document.querySelector("#aluno");
+objeto.textContent = aluno1.nome;
